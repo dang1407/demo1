4aa05a10004b63836000107b0361499955f3af9f
dang1407 <88099558+dang1407@users.noreply.github.com>
2022-01-17 10:02:57 +0700

    Update ax1+..+axn=M.cpp

--- ax1+..+axn=M.cpp
+++ ax1+..+axn=M.cpp
@@ -1,54 +1,40 @@
-#include<bits/stdc++.h>
-
+#include <iostream>
+#define MAX 100001
 using namespace std;
+int x[100],n=30,k=5,s=0,a[100]={0,1,2,3,4,5}, t[6]={0};
 
-int x[100]={0},a[100],n,s,M,dem=0;
-
-void display(int x[100], int n){
-   for(int i=1;i<=n;i++){
-    cout << x[i] << " ";
-   }
-   cout << "\n";
+void print(int a[100], int k){
+	for(int i=1;i<=k;i++){
+		cout << x[i] << " ";
+	}
+	cout << endl;
 }
-int tong(int x[100], int k){
-   int s=0;
-   for(int i=1;i<=k;i++){
-    s+=x[i];
-   }
-   return s;
-}
-void Try(int k){
-
-    for(int v=1; v<= M;v++){
-        s = tong(x,k-1);
-        if(s + v*a[k] <= M){
-            x[k] = v;
-            if(k==n && tong(x,k)==M) {
-              display(x,k);
-              dem=1;
-            } else if(k==n && s!=M) continue;
-            else Try(k+1);
-            s -= v*a[k];
-        } else return;
-    }
+void Try(int i){
+     for(int v=0;v<=n;v++){
+     	if(t[i-1] + a[i]*v<=n){
+     		x[i] = v;
+     		t[i]= t[i-1] +a[i]*v;
+     		if(i==k && t[i]==n) print(x,k);
+     		else if(i==k && t[i]!=n) continue;
+     		else Try(i+1);
+		 } else return;
+	 }
 }
 
-
+void printBinary(int i){
+	for(int v=0;v<=1;v++){
+		x[i] = v;
+		if(i==n) print(x,n);
+		else printBinary(i+1);
+	}
+}
 
 int main(){
-    n=5,M=15;
-    for(int i=1;i<=5;i++){
-        a[i]=1;
-    }
-    cout << "Giai phuong trinh: ";
-    for(int i =1;i<=n;i++){
-        cout << a[i] << "x" << i;
-        if(i<n) cout << " + ";
-    }
-    cout << " = " << M <<endl;
-
-    x[0]=0;
-    Try(1);
-    if(dem==0) cout << "Phuong trinh vo nghiem!";
+	cout << "Giai phuong trinh: ";
+	for(int i=1;i<=k;i++){
+		cout << a[i] << "x" << i;
+		if(i<k) cout << " + ";
+	}
+	cout << " = " << n <<endl;
+	Try(1);
 }
-
